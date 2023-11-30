cb0e72260494d985b6fa79a79eadff5498ccd716
n9vaesss <caioo24332433@gmail.com>
2023-11-30 10:22:46 -0300

    created project

---
+++ src/main/java/com/projectfalteiro/falteiro/FalteiroApplication.java
@@ -0,0 +1,13 @@
+package com.projectfalteiro.falteiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalteiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalteiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectfalteiro/falteiro/FalteiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfalteiro.falteiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalteiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
